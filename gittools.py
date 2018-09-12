7c560c84c917f39e46a46e92c38586ad76454bf4
POFY <pofy_jerry@hotmail.com>
2018-09-12 16:37:44 +0800

    重构部分代码

--- gittools.py
+++ gittools.py
@@ -8,8 +8,9 @@ def checkGitRepoStatus():
     print('获取当前目录:', os.path.abspath(os.path.dirname(__file__)))
     execCmd = ['git', 'status']
     print('开始检查git工作区状态...')
-    returnCode = executeCommand(execCmd)
-    result = checkExecutionResult(returnCode, '获取git工作区状态')
+    returnCode, stdout, stderr = executeCommand(execCmd)
+    result = checkExecutionResult(returnCode, '获取git工作区状态', stdout, stderr)
+    print(stdout)
     return result
 
 
@@ -17,8 +18,8 @@ def checkGitRepoStatus():
 def addAllChangesToStatge():
     execCmd = ['git', 'add', '.']
     print('开始将更新后的文件添加到工作区...')
-    returnCode = executeCommand(execCmd)
-    result = checkExecutionResult(returnCode, '将更新后的文件添加到工作区')
+    returnCode, stdout, stderr = executeCommand(execCmd)
+    result = checkExecutionResult(returnCode, '将更新后的文件添加到工作区', stdout, stderr)
     return result
 
 
@@ -28,8 +29,8 @@ def commitChanges():
         'git', 'commit', '-m', '"auto commit by gittools powered by POFY"'
     ]
     print('准备提交更新至本地库...')
-    returnCode = executeCommand(execCmd)
-    result = checkExecutionResult(returnCode, '提交更新至本地库')
+    returnCode, stdout, stderr = executeCommand(execCmd)
+    result = checkExecutionResult(returnCode, '提交更新至本地库', stdout, stderr)
     return result
 
 
@@ -37,8 +38,9 @@ def commitChanges():
 def checkLocalBranch():
     execCmd = ['bash', 'sh/checkLocalBranch.sh']
     print('准备查看本地分支...')
-    returnCode = executeCommand(execCmd)
-    result = checkExecutionResult(returnCode, '查看本地分支')
+    returnCode, stdout, stderr = executeCommand(execCmd)
+    result = checkExecutionResult(returnCode, '查看本地分支', stdout, stderr)
+    print(stdout)
     return result
 
 
@@ -46,8 +48,8 @@ def checkLocalBranch():
 def checkoutBranch(branchName):
     execCmd = ['git', 'checkout', branchName]
     print('准备切换分支...')
-    returnCode = executeCommand(execCmd)
-    result = checkExecutionResult(returnCode, '切换分支')
+    returnCode, stdout, stderr = executeCommand(execCmd)
+    result = checkExecutionResult(returnCode, '切换分支', stdout, stderr)
     return result
 
 
@@ -55,8 +57,8 @@ def checkoutBranch(branchName):
 def createLocalBranch(branchName):
     execCmd = ['git', 'checkout', '-b', branchName]
     print('准备创建本地分支...')
-    returnCode = executeCommand(execCmd)
-    result = checkExecutionResult(returnCode, '创建本地分支')
+    returnCode, stdout, stderr = executeCommand(execCmd)
+    result = checkExecutionResult(returnCode, '创建本地分支', stdout, stderr)
     return result
 
 
@@ -65,44 +67,45 @@ def deleteLocalBranch(branchName):
     checkoutBranch('master')
     execCmd = ['git', 'branch', '-D', branchName]
     print('准备删除本地分支...')
-    returnCode = executeCommand(execCmd)
-    result = checkExecutionResult(returnCode, '删除本地分支')
+    rreturnCode, stdout, stderr = executeCommand(execCmd)
+    result = checkExecutionResult(returnCode, '删除本地分支', stdout, stderr)
     return result
 
 
 # 为master创建TAG
 def createTag(tagName):
-    execCmd = 'git checkout master && git tag ' + tagName
+    checkoutBranch('master')
+    execCmd = ['git', 'tag', tagName]
     print('准备创建Tag...')
-    returnCode = executeCommand(execCmd)
-    result = checkExecutionResult(returnCode, '创建Tag')
+    returnCode, stdout, stderr = executeCommand(execCmd)
+    result = checkExecutionResult(returnCode, '创建Tag', stdout, stderr)
     return result
 
 
 # 删除tag
 def deleteTag(tagName):
-    execCmd = 'git tag -d ' + tagName
+    execCmd = ['git', 'tag', '-d', tagName]
     print('准备删除Tag...')
-    returnCode = executeCommand(execCmd)
-    result = checkExecutionResult(returnCode, '删除Tag')
+    returnCode, stdout, stderr = executeCommand(execCmd)
+    result = checkExecutionResult(returnCode, '删除Tag', stdout, stderr)
     return result
 
 
 # 推送tag至远端
 def pushTag(tagName):
-    execCmd = 'git push origin ' + tagName
+    execCmd = ['git', 'push', 'origin', tagName]
     print('准备推送Tag至远端...')
-    returnCode = executeCommand(execCmd)
-    result = checkExecutionResult(returnCode, '推送Tag至远端')
+    returnCode, stdout, stderr = executeCommand(execCmd)
+    result = checkExecutionResult(returnCode, '推送Tag至远端', stdout, stderr)
     return result
 
 
 # 更新远端索引
 def fetchIndex():
-    execCmd = 'git fetch'
+    execCmd = ['git', 'fetch']
     print('准备更新远端索引...')
-    returnCode = executeCommand(execCmd)
-    result = checkExecutionResult(returnCode, '更新远端索引')
+    returnCode, stdout, stderr = executeCommand(execCmd)
+    result = checkExecutionResult(returnCode, '更新远端索引', stdout, stderr)
     return result
 
 
@@ -115,35 +118,34 @@ def executeCommand(execCmd):
             execCmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
         # print('output string:', childProcess.stdout.read())
         stdout, stderr = childProcess.communicate()
-        print('-----stdout begin-----')
-        print(stdout)
-        print('-----stdout end  -----\n')
-        print('-----stderr begin-----')
-        print(stderr)
-        print('-----stderr end  -----\n')
-        # childProcess.wait()
+        childProcess.wait()
         print('-----------------command result end.   ----------------')
-        print('命令执行完成,exit with code:', childProcess.returncode)
-        return childProcess.returncode
+        returnCode = childProcess.returncode
+        print('命令执行完成,exit with code:', returnCode)
+        return returnCode, str(stdout, 'UTF-8'), str(stderr, 'UTF-8')
     finally:
         childProcess.kill()
 
 
 # 检查命令执行结果
-def checkExecutionResult(returnCode, message):
+def checkExecutionResult(returnCode, message, stdout, stderr):
     if returnCode != 0:
-        print(message, '--', '失败\n')
+        print(message, '--', '失败!\n')
+        print('结果')
+        print(stderr)
         return False
     else:
-        print(message, '--', '成功\n')
+        print(message, '--', '成功!\n')
+        print('结果')
+        print(stdout)
         return True
 
 
 def main():
-    if (checkGitRepoStatus()):
-        if (addAllChangesToStatge()):
-            commitChanges()
-    # checkLocalBranch()
+    # if (checkGitRepoStatus()):
+    #     if (addAllChangesToStatge()):
+    #         commitChanges()
+    checkLocalBranch()
     # createLocalBranch('dev')
     # deleteLocalBranch('dev')
     # executeCommand('')
